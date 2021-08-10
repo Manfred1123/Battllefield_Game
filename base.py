ff1f82cf02456fc6cad364a9f190c63c24677b4d
Manfred <74629388+Manfred1123@users.noreply.github.com>
2021-08-10 22:38:05 +0000

    Update base.py

--- base.py
+++ base.py
@@ -22,13 +22,22 @@ ship_col = random_col(board)
 print ship_row
 print ship_col
 
-guess_row = int(raw_input("Guess Row: "))
-guess_col = int(raw_input("Guess Col: "))
+# Everything from here on should be in your for loop
+# don't forget to properly indent!
+for turn in range(4):
+  print "Turn", turn + 1
+  guess_row = int(raw_input("Guess Row: "))
+  guess_col = int(raw_input("Guess Col: "))
 
-# Write your code below!
-if guess_row == ship_row and guess_col == ship_col:
-  print("Congratulations! You sank my battleship")
-else:
-  print("You missed my battleship!")
-  board[guess_row][guess_col] = "X"
-  print_board(board)
+  if guess_row == ship_row and guess_col == ship_col:
+    print "Congratulations! You sank my battleship!"   
+  else:
+    if guess_row not in range(5) or \
+      guess_col not in range(5):
+      print "Oops, that's not even in the ocean."
+    elif board[guess_row][guess_col] == "X":
+      print( "You guessed that one already." )
+    else:
+      print "You missed my battleship!"
+      board[guess_row][guess_col] = "X"
+    print_board(board)
